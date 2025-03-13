28fc097f522def1a33b0b31d1080f37e90cbaf7d
AdamaDofara <adamasilue572@gmail.com>
2025-03-13 23:38:39 +0100

    Alert

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,24 @@ export function App() {
   return (
     <div className={s.main_container}>
       <div className={`row ${s.header}`}>
-        <div className={`col-3`}><Logo title={"ISpent"} subtitle={"Track yourexpenses"}/></div>
+        <div className={`col-3`}>
+          <Logo title={"ISpent"} subtitle={"Track yourexpenses"} />
+        </div>
         <div className={`col-9 ${s.income_input}`}>
-         <IncomeInput/>
+          <IncomeInput />
         </div>
       </div>
       <div className={`row ${s.workspace}`}>
-        <div className={`col-12  ${s.expense_input}`}> <ExpenseInput /></div>
-        <div className={`col-12 ${s.alert}`}>
-        <Alert/>
+        <div className={`col-12  ${s.expense_input}`}>
+          {" "}
+          <ExpenseInput />
         </div>
+        <div className={`col-12 ${s.alert}`}>{/* <Alert/> */}</div>
         <div className={`col-11 col-md-6 col-lg-4 ${s.expense_list}`}>
           <List items={expenseList} />
-          <div className={`col-12 ${s.expense_total}`}><ExpenseTotal/></div>
+          <div className={`col-12 ${s.expense_total}`}>
+            <ExpenseTotal />
+          </div>
         </div>
       </div>
     </div>
